c76e88d49b2679bfa88ea730f0a29b6f7a080518
ssmarika <smarikashrestha02@gmail.com>
2024-07-23 16:08:31 +0545

    Made it easier

--- middleware/authentication.middleware.js
+++ middleware/authentication.middleware.js
@@ -39,89 +39,90 @@ export const isUser = async (req, res, next) => {
 };
 
 export const isSeller = async (req, res, next) => {
-  // extract token from req.headers
-  const { authorization } = req.headers;
-
-  const splittedArray = authorization?.split(" ");
-
-  const token = splittedArray?.length === 2 ? splittedArray[1] : null;
-
-  // if not token, throw error
-  if (!token) {
-    return res.status(401).send({ message: "Unauthorized." });
-  }
-
-  let payload;
   try {
+    // extract token from req.headers
+
+    const { authorization } = req.headers;
+
+    const splittedArray = authorization?.split(" ");
+
+    const token = splittedArray?.length === 2 ? splittedArray[1] : null;
+
+    // if not token ,throw error
+    if (!token) {
+      throw new Error();
+      //here the error is an object , () holds the message
+    }
+
     const secretKey = process.env.ACCESS_TOKEN_SECRET_KEY;
 
-    // decrypt token
-    payload = jwt.verify(token, secretKey);
+    //  verify token
+    const payload = jwt.verify(token, secretKey);
+
+    // find user using email from payload
+    const user = await User.findOne({ email: payload.email });
+
+    // if not user found, throw error
+    if (!user) {
+      throw new Error();
+    }
+
+    // check if user role is "seller"
+    //  if user role is not "seller", throw error
+    if (user.role !== "seller") {
+      throw new Error();
+    }
+
+    // attach user._id to req
+    req.loggedInUserId = user._id;
+
+    // call next function
+    next();
   } catch (error) {
-    // if decryption fails, throw error
     return res.status(401).send({ message: "Unauthorized." });
   }
-
-  // find user using email from payload
-  const user = await User.findOne({ email: payload.email });
-
-  // if not user, throw error
-  if (!user) {
-    return res.status(401).send({ message: "Unauthorized." });
-  }
-
-  // if user role is not seller, throw error
-  if (user.role !== "seller") {
-    return res.status(401).send({ message: "Unauthorized." });
-  }
-
-  // add user._id as loggedInUserId
-  req.loggedInUserId = user._id;
-
-  // call next function
-  next();
 };
 
 export const isBuyer = async (req, res, next) => {
-  // extract token from req.headers
-  const { authorization } = req.headers;
-
-  const splittedArray = authorization?.split(" ");
-
-  const token = splittedArray?.length === 2 ? splittedArray[1] : null;
-
-  // if not token, throw error
-  if (!token) {
-    return res.status(401).send({ message: "Unauthorized." });
-  }
-
-  let payload;
   try {
+    // extract token from req.headers
+
+    const { authorization } = req.headers;
+
+    const splittedArray = authorization?.split(" ");
+
+    const token = splittedArray?.length === 2 ? splittedArray[1] : null;
+
+    // if not token ,throw error
+    if (!token) {
+      throw new Error();
+    }
+
     const secretKey = process.env.ACCESS_TOKEN_SECRET_KEY;
 
-    // decrypt token
-    payload = jwt.verify(token, secretKey);
+    //  verify token
+    const payload = jwt.verify(token, secretKey);
+
+    // find user using email from payload
+    const user = await User.findOne({ email: payload.email });
+
+    // if not user found, throw error
+    if (!user) {
+      throw new Error();
+    }
+
+    // check if user role is "buyer"
+    //  if user role is not "buyer", throw error
+    if (user.role !== "buyer") {
+      throw new Error();
+    }
+
+    // attach user._id to req
+    req.loggedInUserId = user._id;
+
+    // call next function
+    next();
   } catch (error) {
-    // if decryption fails, throw error
     return res.status(401).send({ message: "Unauthorized." });
   }
-
-  // find user using email from payload
-  const user = await User.findOne({ email: payload.email });
-
-  // if not user, throw error
-  if (!user) {
-    return res.status(401).send({ message: "Unauthorized." });
-  }
-
-  // if user role is not seller, throw error
-  if (user.role !== "buyer") {
-    return res.status(401).send({ message: "Unauthorized." });
-  }
-
-  // add user._id as loggedInUserId
-  req.loggedInUserId = user._id;
-
-  // call next function
-  next();
 };
